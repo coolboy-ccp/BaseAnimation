c38e1272b1ff28c05e29bdc1d404b84af31c9181
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2015-08-12 14:13:11 +0800

    Initial Commit

---
+++ BaseAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseAnimation
+//
+//  Created by liqunfei on 15/8/12.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaseAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaseAnimation
+//
+//  Created by liqunfei on 15/8/12.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
